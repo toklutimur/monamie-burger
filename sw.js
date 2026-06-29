5c8298d77194183b70316bfaac334b89ee28a73d
toklutimur <toklutimur@gmail.com>
2026-06-29 22:03:04 +0200

    Force refresh cached site assets

--- sw.js
+++ sw.js
@@ -7,7 +7,20 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((keys) => {
       return Promise.all(keys.map((key) => caches.delete(key)));
     }).then(() => {
+      return self.clients.claim();
+    }).then(() => {
+      return self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    }).then((clients) => {
+      clients.forEach((client) => {
+        if ('navigate' in client) {
+          client.navigate(client.url);
+        }
+      });
       return self.registration.unregister();
     })
   );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
